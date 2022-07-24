79c600824536eee7fa3f061068a8ce9727e297de
Giovanni Prisco <gprisco01@gmail.com>
2022-07-24 11:32:49 +0200

    Added onDelete

--- Bookrific/Chat/ChatListView.swift
+++ Bookrific/Chat/ChatListView.swift
@@ -11,23 +11,28 @@ struct ChatListView: View {
     @ObservedObject var conversationManager = ConversationManager.shared
     
     var body: some View {
-        List($conversationManager.conversations) { $chat in
-            NavigationLink(destination: ChatView(conversation: $chat)) {
-                HStack {
-                    Image(systemName: "person.fill")
-                        .resizable()
-                        .frame(width: 40, height: 40, alignment: .center)
-                        .cornerRadius(20)
-
-                    VStack(alignment: .leading) {
-                        Text("\(chat.recipient.name) \(chat.recipient.surname)")
-                            .bold()
+        List {
+            ForEach($conversationManager.conversations) { $chat in
+                NavigationLink(destination: ChatView(conversation: $chat)) {
+                    HStack {
+                        Image(systemName: "person.fill")
+                            .resizable()
+                            .frame(width: 40, height: 40, alignment: .center)
+                            .cornerRadius(20)
                         
-                        Text((chat.messages.last!.from == nil ? "Tu: " : "") + (chat.messages.last!.message ?? "\(chat.messages.last!.attachments!.count) Allegati"))
-                            .fontWeight(.light)
+                        VStack(alignment: .leading) {
+                            Text("\(chat.recipient.name) \(chat.recipient.surname)")
+                                .bold()
+                            
+                            Text((chat.messages.last!.from == nil ? "Tu: " : "") + (chat.messages.last!.message ?? "\(chat.messages.last!.attachments!.count) Allegati"))
+                                .fontWeight(.light)
+                        }
                     }
                 }
             }
+            .onDelete(perform: { offsets in
+                conversationManager.conversations.remove(atOffsets: offsets)
+            })
         }
         .navigationTitle("Chat")
     }
